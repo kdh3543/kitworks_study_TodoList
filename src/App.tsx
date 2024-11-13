664c5299f20d0226dcf8c29e2863418bd932fdeb
Donghyun Kim <91539013+kdh3543@users.noreply.github.com.>
2024-11-13 14:43:45 +0900

    fix: todo 반응형 사이즈 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { Fragment } from 'react';
-import TodoHeader from './components/TodoHeader';
-import TodoContent from './components/TodoContent';
-import TodoFooter from './components/TodoFooter';
+import React, { Fragment } from "react";
+import TodoHeader from "./components/TodoHeader";
+import TodoContent from "./components/TodoContent";
+import TodoFooter from "./components/TodoFooter";
 
 function App() {
   return (
     <Fragment>
       <div className="w-[100%] h-[100vh] flex justify-center items-center">
-        <div className="w-[30%] h-[600px] border-2 bg-white p-5 rounded-2xl shadow-lg shadow-[#537c9b] relative">
+        <div className="w-[100%] xl:w-[30%] md:w-[30%] sm:w-[80%] h-[600px] border-2 bg-white p-5 rounded-2xl shadow-lg shadow-[#537c9b] relative">
           <TodoHeader />
           <hr className="border-1 border-black h-[5%] mt-2" />
           <TodoContent />
