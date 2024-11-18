943a15bb40e0ebc6f9c70ee47c53882f6790d37e
Donghyun Kim <91539013+kdh3543@users.noreply.github.com.>
2024-11-18 17:27:20 +0900

    feat: json server 추가 테스트 중

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,28 @@ function App() {
         return res.json();
       })
       .then((data) => {
-        setId(data[0].id);
+        setId(data[0].content);
         console.log(data);
       });
   }, [url]);
+
+  const addTest = () => {
+    const param = {
+      title: "1234",
+      content: 1234,
+    };
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    });
+  };
   return (
     <div className="w-[100%] h-[100vh] flex justify-center items-center">
       <div className="w-[100%] xl:w-[30%] md:w-[30%] sm:w-[80%] h-[600px] border-2 bg-white p-5 rounded-2xl shadow-lg shadow-[#537c9b] relative">
-        {id}
+        <button onClick={addTest}>add</button>
         <TodoHeader />
         <hr className="border-1 border-black h-[5%] mt-2" />
         <TodoContent />
